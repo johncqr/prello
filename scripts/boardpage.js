91b380b432a2cc278ef2cc5d6ac1d4f2f1f5b55a
John Reyes <johncqr@gmail.com>
2017-06-21 12:51:16 -0700

    completed modal

--- scripts/boardpage.js
+++ scripts/boardpage.js
@@ -1,12 +1,29 @@
 function toggleBoardsLists() {
-    var boardsList = document.getElementById("boards-list");
-    boardsList.style.display = boardsList.style.display === "none" ? "block" : "none";
+    var boardsList = document.getElementById('boards-list');
+    console.log(boardsList.style.display);
+    boardsList.style.display = boardsList.style.display === 'none' ? 'block' : 'none'
 }
 
 function toggleBoardMenu() {
-    var boardMenu = document.getElementById("board-menu");
-    boardMenu.style.display = boardMenu.style.display === "none" ? "block" : "none";
+    var boardMenu = document.getElementById('board-menu');
+    boardMenu.style.display = boardMenu.style.display === 'none' ? 'block' : 'none'
 }
 
-document.getElementById("boards-list-btn").addEventListener("click", toggleBoardsLists);
-document.getElementById("board-menu-btn").addEventListener("click", toggleBoardMenu);
+function openNewCard() {
+    var newCard = document.querySelector('.modal');
+    newCard.style.display = '';
+}
+
+function closeNewCard() {
+    var newCard = document.querySelector('.modal');
+    newCard.style.display = 'none';
+}
+
+document.getElementById('boards-list').style.display = 'none';
+document.getElementById('board-menu').style.display = 'none';
+
+document.querySelector('#boards-list-btn').addEventListener('click', toggleBoardsLists);
+document.getElementById('board-menu-btn').addEventListener('click', toggleBoardMenu);
+document.querySelector('.add-link').addEventListener('click', openNewCard);
+document.querySelector('.modal-bg').addEventListener('click', closeNewCard);
+document.querySelector('.close-new-card-btn').addEventListener('click', closeNewCard);
