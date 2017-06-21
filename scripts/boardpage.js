8558475fac51989a39c037bc879c06e8a3881be7
John Reyes <johncqr@gmail.com>
2017-06-20 22:46:55 -0700

    complete menus base

--- scripts/boardpage.js
+++ scripts/boardpage.js
@@ -3,4 +3,10 @@ function toggleBoardsLists() {
     boardsList.style.display = boardsList.style.display === "none" ? "block" : "none";
 }
 
+function toggleBoardMenu() {
+    var boardMenu = document.getElementById("board-menu");
+    boardMenu.style.display = boardMenu.style.display === "none" ? "block" : "none";
+}
+
 document.getElementById("boards-list-btn").addEventListener("click", toggleBoardsLists);
+document.getElementById("board-menu-btn").addEventListener("click", toggleBoardMenu);
