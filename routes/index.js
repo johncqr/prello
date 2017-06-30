bd07cd47b23cf014474c92e21defd9fe045837ba
John Reyes <johncqr@gmail.com>
2017-06-30 10:53:48 -0700

    index serves board page

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-router.get('/test', function(req, res, next) {
-  res.send("Hello!");
+  res.render('index', { title: 'Board Page' });
 });
 
 module.exports = router;
