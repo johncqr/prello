6118af1ed9af3a56ae5a42d6d6d9d78647720bc4
John Reyes <johncqr@gmail.com>
2017-06-30 12:24:37 -0700

    remove needless files

--- scripts/boards.js
+++
@@ -1,7 +0,0 @@
-function toggleBoardsLists() {
-    var boardsList = document.getElementById("boards-list");
-    boardsList.style.display = boardsList.style.display === "none" ? "block" : "none";
-}
-
-document.getElementById('boards-list').style.display = 'none';
-document.getElementById("boards-list-btn").addEventListener("click", toggleBoardsLists);
