55b1823e7bd45d5db1313c3d3618c57b54db02e7
John Reyes <johncqr@gmail.com>
2017-06-30 10:39:17 -0700

    remove mongo version key

--- models/card.js
+++ models/card.js
@@ -4,6 +4,6 @@ var schema = new Schema({
     name: String,
     desc: String,
     labels: Array,
-});
+}, { versionKey: false});
 
 module.exports = mongoose.model('Card', schema);
--- models/list.js
+++ models/list.js
@@ -3,6 +3,6 @@ var Schema = mongoose.Schema;
 var schema = new Schema({
     name: String,
     cards: Array,
-});
+}, { versionKey: false });
 
 module.exports = mongoose.model('List', schema);
