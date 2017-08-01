abe4437c8ea440c3cd69bffc6d3fe1b47ca4afde
John Reyes <johncqr@gmail.com>
2017-08-01 12:20:31 -0700

    add new card in react

--- src/boardpage.js
+++ src/boardpage.js
@@ -97,32 +97,6 @@ $(function () {
     function toggleBoardMemberAddMenu() {
         $boardMemberAddMenu.toggle();
     }
-    function openNewCard() {
-        $newCardModal.show();
-        $newCardNameInput.focus();
-    }
-
-    function closeNewCard() {
-        $newCardModal.hide();
-        $newCardNameInput.val('');
-        $newCardDescInput.val('');
-    }
-
-    // ajax calls
-    function addNewCard() {
-        if ($newCardNameInput.val() != '') {
-            $.ajax({
-                url: `${HOST}/list/${currentLid}/card/`,
-                data: {
-                    name: $newCardNameInput.val(),
-                    desc: $newCardDescInput.val(),
-                },
-                type: 'POST',
-                dataType: 'json',
-            });
-            closeNewCard();
-        }
-    }
 
     function addNewLabel() {
         var labelData = {
@@ -244,8 +218,7 @@ $(function () {
         }
 
         handleClickCardAddLink() {
-            currentLid = this.props.lid;
-            openNewCard();
+            this.props.onOpenCardAdder(this.props.lid);
         }
 
         handleKeypress(e) {
@@ -317,6 +290,52 @@ $(function () {
             );
         }
 
+    }
+
+    class NewModal extends React.Component {
+        constructor() {
+            super();
+            this.state = {
+                name: '',
+                desc: '',
+            }
+        }
+
+        handleNameChange(e) {
+            this.setState({ name: e.target.value });
+        }
+
+        handleDescChange(e) {
+            this.setState({ desc: e.target.value });
+        }
+
+        handleSubmit() {
+            this.props.onAddCard(this.state);
+        }
+
+        render() {
+            return (
+                <div id="new-card-modal" className="modal">
+                    <div id="new-card-page" className="card-page">
+                        <div className="card-page-topbar">
+                            <div className="card-page-topbar-left">
+                                <input id="new-card-name-input" value={this.state.name} onChange={this.handleNameChange.bind(this)} className="card-page-name" type="text" placeholder="Enter card name..." required />
+                            </div>
+                            <div className="card-page-topbar-right">
+                                <div id="close-new-card-btn" className="btn" onClick={this.props.onCloseNewCard}>X</div>
+                            </div>
+                        </div>
+                        <div className="card-info">
+                            <p className="card-section-name">Description</p>
+                            <textarea id="new-card-desc-input" value={this.state.desc} onChange={this.handleDescChange.bind(this)} rows="3" cols="35" placeholder="Enter description..." />
+                        </div>
+                        <div className="div-clearer"></div>
+                        <p id="add-card-btn" className="btn" onClick={this.handleSubmit.bind(this)}>Add Card</p>
+                    </div>
+                    <div id="new-card-modal-bg" className="modal-bg"></div>
+                </div>
+            );
+        }
     }
 
     class InfoModal extends React.Component {
@@ -579,6 +598,19 @@ $(function () {
             });
         }
 
+        handleAddCard(cardData) {
+            $.ajax({
+                url: `${HOST}/list/${this.state.currentLid}/card/`,
+                data: {
+                    name: cardData.name,
+                    desc: cardData.desc,
+                },
+                type: 'POST',
+                dataType: 'json',
+            });
+            this.setState({ newCardModalOpen: false });
+        }
+
         handleListNameChange(lid, name) {
             $.ajax({
                 url: `${HOST}/list/${lid}`,
@@ -607,6 +639,12 @@ $(function () {
         handleCloseCard() {
             this.setState({
                 cardModalOpen: false,
+            });
+        }
+
+        handleCloseNewCard() {
+            this.setState({
+                newCardModalOpen: false,
             });
         }
 
@@ -648,12 +686,20 @@ $(function () {
             });
         }
 
+        handleOpenCardAdder(lid) {
+            this.setState({
+                currentLid: lid,
+                newCardModalOpen: true,
+            });
+        }
+
         render() {
             let lists = this.state.data.map((l) => {
                 return <List key={l._id} lid={l._id} name={l.name} cards={l.cards}
                     onOpenCard={this.handleOpenCard.bind(this)}
                     onDeleteList={this.handleDeleteList.bind(this)}
-                    onListNameChange={this.handleListNameChange.bind(this)} />
+                    onListNameChange={this.handleListNameChange.bind(this)}
+                    onOpenCardAdder={this.handleOpenCardAdder.bind(this)} />
             });
 
             let cardData;
@@ -680,6 +726,13 @@ $(function () {
                     onCardDescChange={this.handleCardDescChange.bind(this)}
                     />
                 }
+                { this.state.newCardModalOpen &&
+                    <NewModal
+                    onAddCard={this.handleAddCard.bind(this)}
+                    onCloseNewCard={this.handleCloseNewCard.bind(this)}
+                    />
+
+                }
                 </div>
             );
         }
@@ -690,9 +743,6 @@ $(function () {
     $('#board-menu-btn').click(openBoardMenu);
     $('#board-menu-close-btn').click(closeBoardMenu);
     $('#add-label-btn').click(toggleAddLabelMenu);
-    $('#new-card-modal-bg').click(closeNewCard);
-    $('#close-new-card-btn').click(closeNewCard);
-    $('#add-card-btn').click(addNewCard);
     $('.add-label-selector').click(addNewLabel);
     $('#board-member-add-menu-btn').click(toggleBoardMemberAddMenu);
     $('#add-board-member-btn').click(addBoardMember);
